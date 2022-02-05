6a2ab6b7bb5707b03974952196353bd3b437843a
Guilherme Scherer <guilherme_scherer@outlook.com>
2022-02-05 14:40:03 -0300

    Implements LOG

--- src/main/java/com/personal/school/validation/impl/DateValidator.java
+++ src/main/java/com/personal/school/validation/impl/DateValidator.java
@@ -2,6 +2,7 @@ package com.personal.school.validation.impl;
 
 import com.personal.school.validation.Date;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -13,6 +14,8 @@ import static com.personal.school.utils.FormatterUtils.getDefaultDateFormatter;
 
 public class DateValidator implements ConstraintValidator<Date, String> {
 
+    private static final Logger LOG = Logger.getLogger(DateValidator.class);
+
     @Override
     public boolean isValid(String date, ConstraintValidatorContext constraintValidatorContext) {
         if(StringUtils.isBlank(date)) return true;
@@ -21,6 +24,7 @@ public class DateValidator implements ConstraintValidator<Date, String> {
             LocalDate.parse(date, getDefaultDateFormatter());
             return true;
         } catch (DateTimeParseException e) {
+            LOG.error(e);
             return false;
         }
 
